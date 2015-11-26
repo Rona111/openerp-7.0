a7f58d2f45775f28365dfc67eda2521e7eccc078
rona <rona.lin@elico-corp.com>
2015-11-26 17:47:46 +0800

    [IMP]10820 add module patch_pos_update_ending_balance.

---
+++ patch_pos_update_ending_balance/pos.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (c) 2010-2014 Elico Corp. All Rights Reserved.
+#    Alex Duan <alex.duan@elico-corp.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from openerp.osv import orm
+
+
+class pos_session(orm.Model):
+    _inherit = 'pos.session'
+
+    def wkf_check_final_balance(self, cr, uid, ids, context=None):
+        for record in self.browse(cr, uid, ids, context=context):
+            for st in record.statement_ids:
+                if st.journal_id.type == 'bank':
+                    st.write(
+                        {'balance_end_real': st.balance_end})
